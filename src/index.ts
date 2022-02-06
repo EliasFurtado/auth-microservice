e8add513a685bb449e54cf604fa5f6ff2d5b71ed
Elias Furtado <eliasfurtado.dev@gmail.com>
2022-02-06 15:50:27 -0300

    create express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('rodou')
+import express, { Request, Response } from 'express'
+
+const PORT = 3000
+const app = express()
+
+app.get('/', (req: Request, res: Response) => {
+    res.status(200).send({name: "elias"})
+})
+
+app.listen(PORT, () => {
+    console.log(`app is running on http://localhost:${PORT}`)
+})
