b60d3d7ed2f815645a4592fcbe53f8b01859e8d4
Elias Furtado <eliasfurtado.dev@gmail.com>
2022-02-09 22:15:13 -0300

    create DatabaseError model

---
+++ src/models/errors/database.error.model.ts
@@ -0,0 +1,11 @@
+class DatabaseError extends Error {
+    constructor(
+        public message: string,
+        public error?: string
+        ) {
+            super(message)
+        }
+
+}
+
+export default DatabaseError
