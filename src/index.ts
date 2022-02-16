436fac8d25d5e4ceba6fe7b67772e7d0c7b744eb
Elias Furtado <eliasfurtado.dev@gmail.com>
2022-02-16 18:03:38 -0300

    create bearer-authentication middleware

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv'
 import express, { Request, Response } from 'express'
+import bearerAuthenticationMiddleware from './middlewares/bearer-authentication.middleware'
 import errorHandler from './middlewares/error-handler.middleware'
 import authorizationRoute from './routes/authorization.routes'
 import usersRoute from './routes/users.routes'
@@ -14,7 +15,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-app.use(usersRoute)
+app.use(bearerAuthenticationMiddleware, usersRoute)
 app.use(authorizationRoute)
 
 app.use(errorHandler)
